cf6e73c6535bd08e6cb09c2338ea6f5ac51e78b4
Himanshu Yadav <himanshu07319@gmail.com>
2024-10-28 23:02:09 -0700

    Add instruction queue member functions

--- src/Instruction_Queue/instruction_queue.hh
+++ src/Instruction_Queue/instruction_queue.hh
@@ -4,8 +4,17 @@
 #include "../Common/common.hh"
 #include "../Entry/entry.hh"
 #include <queue>
-class InstructionQueue{
+
+class InstructionQueueDS{
     std :: queue <Instruction_Queue_Entry> instructionQ;
+
+    public:
+        InstructionQueueDS(){};
+        void createInstructionQ();
+        bool isEmpty();
+
+        Instruction_Queue_Entry getEntry();
+        void popInstQ();
 
 };
 

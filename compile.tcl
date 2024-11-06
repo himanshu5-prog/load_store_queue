2d7ace5e20546f9e4a36d153365417c3074b8532
Himanshu Yadav <himanshu07319@gmail.com>
2024-11-06 12:40:39 -0800

    Add compile command file
    - Use 'source compile.tcl' to compile

---
+++ compile.tcl
@@ -0,0 +1,9 @@
+g++ \
+src/mainFunc.cpp \
+src/system/system.cc \
+src/common/common.cc \
+src/Entry/entry.cc \
+src/Instruction_Queue/instruction_queue.cc \
+src/Load_Store_Queue/common_queue.cc \
+-o lsq
+

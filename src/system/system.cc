329d90104088473a19124ea17f37a5069ca1af45
Himanshu Yadav <himanshu07319@gmail.com>
2024-11-06 15:15:14 -0800

    Update system class

--- src/system/system.cc
+++ src/system/system.cc
@@ -2,16 +2,34 @@
 
 System :: System (){
     cycleCount = 0;
-    loadQ.setInstancename("Load Queue");
-    storeQ.setInstancename("Store Queue");
     instQ_DS.createInstructionQ();
-
     cycleCount = 0;
     maxCycleCount = 100;
+    debugMode = false;
 }
 
 void System :: run(){
+    //int currentCycleCount;
+    for (int currentCycleCount = 0; currentCycleCount < maxCycleCount; ++currentCycleCount){
 
+        if (debugMode){
+            std :: cout << "System:: run(): Cycle : " << cycleCount << "\n";
+            std :: cout << " Pre-processing entry: \n";
+            loadStoreQueue.printAllEntry();
+        }
+
+
+        loadStoreQueue.retireEntryHead();
+        loadStoreQueue.updateAllLoadEntry();
+        loadStoreQueue.UpdateEntry();
+        issue();
+
+        if (allBufferEmpty()){
+            break;
+        }
+        increment_cycle_count();
+
+    }
 }
 
 void System :: issue (){
@@ -20,37 +38,38 @@ void System :: issue (){
         Instruction_Queue_Entry instEntry;
         instEntry = instQ_DS.getEntry();
         assert(instEntry.valid);
-        if (instEntry.opcode == LOAD && loadQ.canAddEntry()){
+        if (instEntry.opcode == LOAD && loadStoreQueue.canAddToEntry()){
             // Adding entry to load queue
-            LSQ_Entry e;
-            e.valid = instEntry.valid;
-            e.opcode = LOAD;
-            e.data = instEntry.data;
-            e.ready = false;
-            e.age = instEntry.id;
-            e.address = instEntry.address;
-            e.startCycle = cycleCount;
-            e.stage = LD_DISPATCH;
-            e.endCycle = cycleCount + Load_Pipeline_Latency::DISPATCH_TO_ADDR_GEN;
-
-            loadQ.addToQueue(e);
+            loadStoreQueue.addToQueue(instEntry.address, instEntry.data, instEntry.id ,LOAD);
             instQ_DS.popInstQ();
 
-        } else if (instEntry.opcode == STORE && storeQ.canAddEntry()){
+        } else if (instEntry.opcode == STORE && loadStoreQueue.canAddToEntry()){
             //Adding entry to store queue
-            LSQ_Entry e;
-            e.valid = instEntry.valid;
-            e.opcode = STORE;
-            e.data = instEntry.data;
-            e.ready = false;
-            e.age = instEntry.id;
-            e.address = instEntry.address;
-            e.startCycle = cycleCount;
-            e.stage = ST_DISPATCH;
-            e.endCycle = cycleCount + Store_Pipeline_Latency::DISPATCH_TO_EXECUTE;
-
-            storeQ.addToQueue(e);
+            loadStoreQueue.addToQueue(instEntry.address, instEntry.data, instEntry.id, STORE);
             instQ_DS.popInstQ();
         }
     }
+}
+
+void System :: setDebugMode(){
+    debugMode = true;
+    //cache.setDebugMode();
+    loadStoreQueue.setDebugMode();
+}
+
+void System :: increment_cycle_count(){
+    cycleCount += 1;
+    loadStoreQueue.incrementCycleCount();
+}
+
+bool System :: allBufferEmpty(){
+
+    if (instQ_DS.isEmpty() && loadStoreQueue.isEmpty()){
+        if (debugMode){
+            std :: cout << "All buffers empty at cycle: " << cycleCount << "\n";
+        }
+        return true;
+    }
+       
+    return false;
 }
--- src/system/system.hh
+++ src/system/system.hh
@@ -3,21 +3,28 @@
 
 #include <iostream>
 #include <cassert>
-#include "../Cache/cache.hh"
 #include "../Load_Store_Queue/load_store_queue.hh"
+#include "../Load_Store_Queue/common_queue.hh"
 #include "../Instruction_Queue/instruction_queue.hh"
 class System {
-    Cache cache;
-    Load_Store_Queue loadQ;
-    Load_Store_Queue storeQ;
+    //SimpleCache cache;
     InstructionQueueDS instQ_DS;
+    CommonQueue loadStoreQueue;
     int cycleCount;
     int maxCycleCount;
+    bool debugMode;
+    std :: string instructionFile;
 
     public:
         System();
+        void setDebugMode();
         void run();
         void issue();
+        void increment_cycle_count();
+        void setMaxCycleCount(int x) { maxCycleCount = x;}
+        bool allBufferEmpty();
+        void setInstructionFile(std :: string f);
+        void readInstructionFile();
 };
 
 #endif
